7a7d057bd62ee402feee6485ba2f28b6991f7033
codeawy <codeawy@gmail.com>
2024-09-28 13:22:32 +0300

    Enhance Middlewares

--- src/app.ts
+++ src/app.ts
@@ -14,19 +14,32 @@ const app = express();
 // Basic middlewares
 app.use(express.json({ limit: "10kb" }));
 app.use(express.urlencoded({ extended: true, limit: "10kb" }));
-app.use(cors());
-
-// Security middlewares
-app.use(helmet());
-const limiter = rateLimit({
-  max: 100,
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  message: "Too many requests from this IP, please try again later.",
-});
-app.use("/api", limiter);
 
 // Logging middleware
 app.use(morgan("dev"));
+
+app.use(
+  cors({
+    // * Assuming your React app is running on port 3000
+    origin: "http://localhost:3000",
+    // * Setting credentials: true in the CORS options tells the server to include the `Access-Control-Allow-Credentials`: true header in its responses.
+    credentials: true,
+  })
+);
+
+// Security middlewares
+app.use(helmet());
+
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  message: {
+    error: "Too Many Requests",
+    message: "Too many requests from this IP, please try again later.",
+    retryAfter: "15 minutes", // 15 minutes
+  },
+});
+app.use(limiter);
 
 // Compression middleware
 app.use(compression());
